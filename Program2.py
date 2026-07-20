bfd319193310e24cb4eba0c5d2dd0b5ce061135e
Srushti Pardeshi <srushtipardeshi164@gmail.com>
2026-07-20 17:36:54 +0530

    Resolve merge conflicts

--- Program2.py
+++ Program2.py
@@ -13,7 +13,7 @@ else:
 
 side=input("Enter the side of square:")
 area=int(side)**2
-
+print("Area of square is:",area)
 # Program to check if the first number is greater than or equal to the second number
 
 a=int(input("Enter the first number:"))
