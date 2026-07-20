5c8b1ea0effd6849880f1e9761af1765b405fe7f
srushti <srushtipardeshi164@gmail.com>
2026-07-20 17:08:57 +0530

    Add files via upload

---
+++ Program2.py
@@ -0,0 +1,15 @@
+# Program to check if the first number is greater than or equal to the second number
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+if a>=b:
+    print("True")
+else:
+    print("False")
+
+#python program to calculate area of square
+
+side=input("Enter the side of square:")
+area=int(side)**2
+print("Area of square is:",area)
---
+++ program1.py
@@ -0,0 +1,22 @@
+#python program 1
+print("Hii guys, my name is Srushhti.")
+
+#Input given in the program
+a=2
+b=3
+c=a+b
+print("sum of 2 numbers is:",c)
+
+#Input given by user
+a=input("num1:")
+b=input("num2:")
+c=int(a)+int(b)
+print("sum of 2 numbers is:",c)
+
+#Input given in different types
+a,b =1,"2"
+c=int(b)
+sum=a+c
+print("sum of 2 numbers is:",sum)
+
+
