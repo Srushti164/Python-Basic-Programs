f83f118964e7e33730effa0c5f4ff0e0b16c5756
Srushti Pardeshi <srushtipardeshi164@gmail.com>
2026-07-20 18:13:01 +0530

    Add program

--- Program2.py
+++ Program2.py
@@ -1,6 +1,5 @@
 
 # Program to check if the first number is greater than or equal to the second number
-
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 
@@ -9,18 +8,23 @@ if a>=b:
 else:
     print("False")
 
-#python program to calculate area of square
 
+#python program to calculate area of square
 side=input("Enter the side of square:")
 area=int(side)**2
 print("Area of square is:",area)
-# Program to check if the first number is greater than or equal to the second number
 
+
+# Program to check whether the greater number among three numbers.
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
+c=int(input("Enter the third number:"))
 
-if a>=b:
-    print("True")
+if a>b and a>c:
+    print("a is greater than b and c")
+elif b>a and b>c:
+    print("b is greater than a and c")
+elif c>a and c>b:
+    print("c is greater than a and b")
 else:
-    print("False")
-
+    print("All numbers are equal")
